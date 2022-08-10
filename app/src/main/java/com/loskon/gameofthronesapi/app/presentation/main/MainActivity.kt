4c234c9aa97e4c67ed8b1a842b4e13f13b2ae21d
Loskon <andreyrochev23@gmail.com>
2022-08-10 21:54:24 +0300

    Added logo and splash screen

--- app/src/main/java/com/loskon/gameofthronesapi/app/presentation/main/MainActivity.kt
+++ app/src/main/java/com/loskon/gameofthronesapi/app/presentation/main/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.loskon.gameofthronesapi.app.presentation.main
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import com.loskon.gameofthronesapi.R
 import com.loskon.gameofthronesapi.app.base.extension.activity.installTaskDescriptionColor
@@ -11,5 +13,9 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onAttachedToWindow() {
         installTaskDescriptionColor(getColor(android.R.color.white))
         super.onAttachedToWindow()
+    }
+
+    companion object {
+        fun makeIntent(context: Context) = Intent(context, MainActivity::class.java)
     }
 }
